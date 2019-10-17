41193fa42fea8f07b7ce9c2240b2a6f8dba29d27
Chad Kaplan <chadkaplan@knights.ucf.edu>
2019-10-17 13:12:22 -0400

    added the grab value for amount of results

--- public/js/index.js
+++ public/js/index.js
@@ -103,5 +103,6 @@ $("#submit-btn").on("click", function (event) {
   var gender = $("#gender").val();
   var ethnicity = $("#ethnicity").val();
   var letter = $("#letter").val().trim();
-  console.log(gender, ethnicity, letter);
+  var resultQuantity = parseInt($("#amount").val());
+  console.log(gender, ethnicity, letter, resultQuantity);
 });

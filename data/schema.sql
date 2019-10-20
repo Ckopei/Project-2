9c2120ba56efef6a75929196de402ba847984ae4
Ckopei <cameron@kopels.com>
2019-10-20 17:25:09 -0400

    fixing schema to build for heroku since sequelize wont work.

--- data/schema.sql
+++ data/schema.sql
@@ -1,26 +1,18 @@
-DROP DATABASE IF EXISTS name_that_baby_db;
-CREATE DATABASE name_that_baby_db;
 
 USE name_that_baby_db;
 
-CREATE TABLE camNames(
-    id INT AUTO_INCREMENT NOT NULL,
-    year YEAR NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    percentage FLOAT(8,7),
-    sex VARCHAR(30),
-    PRIMARY KEY(id)
-);
 
 
 CREATE TABLE chadNames(
     id INT AUTO_INCREMENT NOT NULL,
-    yearOfBirth YEAR NOT NULL,
-    sex VARCHAR (255) NOT NULL,
-    ethnicity VARCHAR (255) NOT NULL,
-    name VARCHAR (255) NOT NULL,
+    Year_of_birth INT NOT NULL,
+    sex TEXT NOT NULL,
+    ethnicity TEXT NOT NULL,
+    name TEXT NOT NULL,
     occurences INT NOT NULL,
-    nameRank INT NOT NULL,
+    name_rank INT NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    updatedAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 )
 

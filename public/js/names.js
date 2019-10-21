41cc8afe49c1b39c58e8e83c67430e1c306bea30
Ckopei <cameron@kopels.com>
2019-10-20 20:07:31 -0400

    click functions were bound to the ID and werent working with dynamic elements. Changed click function to target the window object first, then the id/class and they not work for all of them. config filed changed as i was testing deploying to heroku with no luck.

--- public/js/names.js
+++ public/js/names.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  $("#removeFromPage").on("click", function () {
+  $(document).on("click", "#removeFromPage", function () {
     //this.closest goes up one level of parent until it finds one with an class of parentRow, then removes it from the page.
     $(this).closest(".parentRow").remove();
   });
 
-  $("#saveThisName").on("click", function () {
+  $(document).on("click", "#saveThisName", function () {
     //insert code for pushing to database here.
     //use $(this).siblings(".supertext").text() to grab the name.
     console.log($(this).siblings(".supertext").text());
